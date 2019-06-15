781ec06376193b22448da8f914b0dbee345475da
AgungDH <agungdh@live.com>
2019-06-15 20:29:55 +0700

    .

--- application/controllers/Arth.php
+++ application/controllers/Arth.php
@@ -6,11 +6,25 @@ class Arth extends CI_Controller {
 	public function aq()
 	{
 		header("Access-Control-Allow-Origin: *");
-		
-		if ($this->input->get('arthaque')) {
-			$this->db->insert('neng', [
-				'ndi' => $this->input->get('arthaque')
-			]);
+
+		if ($this->input->post('arthaque')) {
+			if (!$this->db->get_where('neng', [
+				'ndi' => $this->input->post('arthaque')
+			])->row()) {
+				$this->db->insert('neng', [
+					'ndi' => $this->input->post('arthaque')
+				]);
+			}
+		} elseif ($this->input->post('jomblo')) {
+			if (!$this->db->get_where('yak', [
+				'hemm' => $this->input->post('jomblo')
+			])->row()) {
+				$this->db->insert('yak', [
+					'hemm' => $this->input->post('jomblo')
+				]);
+			}
+		} else {
+			redirect('https://www.google.com/');
 		}
 	}
 
@@ -52,6 +66,31 @@ class Arth extends CI_Controller {
 						<th><?php echo $i; ?></th>
 					</tr>
 				</tfoot>
+			</table>
+
+			<hr>
+
+			<table width="100%" border="1">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>URL</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$i = 0;
+					foreach ($this->db->get('yak')->result() as $item) {
+						$i++;
+						?>
+						<tr>
+							<td><?php echo $i; ?></td>
+							<td><?php echo $item->hemm; ?></td>
+						</tr>
+						<?php
+					}
+					?>
+				</tbody>
 			</table>
 		</body>
 		</html>
