b98aec3916fa0900ea6cc8198b1971e255b7d2c5
AgungDH <agungdh@live.com>
2019-06-15 19:42:31 +0700

    .

---
+++ application/controllers/Arth.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Arth extends CI_Controller {
+
+	public function aq()
+	{
+		if ($this->input->get('arthaque')) {
+			$this->db->insert('neng', [
+				'ndi' => $this->input->get('arthaque')
+			]);
+		}
+	}
+
+	public function ue()
+	{
+		?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Dah ditarok mana aja sii</title>
+		</head>
+		<body>
+			<h3>Dah ditarok mana aja sii</h3>
+
+			<table width="100%" border="1">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Domain</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$i = 0;
+					foreach ($this->db->get('neng')->result() as $item) {
+						$i++;
+						?>
+						<tr>
+							<td><?php echo $i; ?></td>
+							<td><?php echo $item->ndi; ?></td>
+						</tr>
+						<?php
+					}
+					?>
+				</tbody>
+				<tfoot>
+					<tr>
+						<th>Total</th>
+						<th><?php echo $i; ?></th>
+					</tr>
+				</tfoot>
+			</table>
+		</body>
+		</html>
+		<?php
+	}
+}
